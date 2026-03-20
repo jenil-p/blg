9b493ba7128ac3680a759e668c8a3414651c94f6
jenil <jenilsakriya612@gmail.com>
2026-03-20 18:13:06 +0530

    fixed path and made public folder public

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,16 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 connectDB();
 
-
-
 const app = express();
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors());
 app.use(express.json());
